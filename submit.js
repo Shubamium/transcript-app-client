d465028e18043eb2fa32af25a637d89813caf904
Vincent Liussyaputra <liusvinv@gmail.com>
2023-03-09 15:31:23 +0700

    Add upload visualization

--- submit.js
+++ submit.js
@@ -1,45 +1,57 @@
 // Form Reference
 const formEl = document.getElementById('upload-form');
 const resultEl = document.getElementById('result');
+
+// Status reference
 const statusEl = document.getElementById('file-status');
+const submitStatusEl = document.getElementById('submit-status');
 
 
 
 // Input reference
 const uploadBtnEl = document.getElementById('uploader');
+const submitBtnEl = document.getElementById('submiter');
 
 // Listen to the submit event
 formEl.addEventListener('submit',handleSubmit);
 uploadBtnEl.addEventListener('change',handleFileChange);
 
-
+disableButton("  ");
 function openUpload(){
     uploadBtnEl.click();
 }
 function handleFileChange(e){
     const file =  e.target.files[0].name;
     statusEl.innerText = file;
+    if(e.target.files[0]){
+        enableButton();
+    }
 }
 
 
 async function handleSubmit(e){
     e.preventDefault();
-    console.log('form submitted');
+
+
+    disableButton();
 
     const formData = new FormData(e.target);
     const file = formData.entries('toTranscribe');
+    if(!file){
+        return;
+    }
     // console.log(formData,file[0]);
     
     // return;
-    const header = {
-        // header
-        // "Application-type":"x"
-    }
-    const response = await fetch('apiroute:port/route',{
-        method:'POST',
-        body:file
-    });
-    let result = await response.json();
+    // const header = {
+    //     // header
+    //     // "Application-type":"x"
+    // }
+    // const response = await fetch('apiroute:port/route',{
+    //     method:'POST',
+    //     body:file
+    // });
+    // let result = await response.json();
 
     // const res = {
     //     success: boolean,
@@ -62,6 +74,27 @@ async function handleSubmit(e){
     }else{
         // Show error message
         resultEl.value="Erorr: " + result.message;
-        
     }
+
+    // Simulate delay
+    setTimeout(()=>{
+        enableButton();
+    },4000)
+}
+
+function disableButton(status){
+    submitBtnEl.setAttribute('disabled','true');
+    submitBtnEl.classList.add('disabled');
+
+    // Status Message
+    submitStatusEl.style.display = 'block';
+    submitStatusEl.innerHTML = status || `<i class="fa-solid fa-spinner fa-beat fa-spin"></i> Transcribing . . . `;
+}
+function enableButton(){
+    submitBtnEl.removeAttribute('disabled');
+    submitBtnEl.classList.remove('disabled');
+
+    // Status Message
+    submitStatusEl.style.display = 'none';
+
 }
